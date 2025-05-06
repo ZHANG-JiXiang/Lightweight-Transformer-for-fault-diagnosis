b11602236e50e65449d839e282a5d56874cec914
Destiny <116323153+ZHANG-JiXiang@users.noreply.github.com>
2025-05-06 12:56:28 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,9 @@
+from.hnu_datasets import HNU_Datasets
+from.xjtu_datasets import XJTU_Datasets
+from.dds_datasets import DDS_Datasets
+from.sqv_datasets import SQV_Datasets
+datasets_dict = {'hnu_datasets': HNU_Datasets,
+                 'xjtu_datasets':XJTU_Datasets,
+                 'dds_datasets':DDS_Datasets,
+                 'sqv_datasets': SQV_Datasets
+                 }
